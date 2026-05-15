760b2051cc8aac8a552141e2ca2a0263dcc705ce
sharathkumar <ssharathkumar033@gmail.com>
2026-05-15 21:20:59 +0530

    Create superuser during deployment

--- build.sh
+++ build.sh
@@ -1,5 +1,37 @@
 #!/usr/bin/env bash
 
 pip install -r requirements.txt
+
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+python manage.py shell <<'PY'
+import os
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+username = os.environ.get("DJANGO_SUPERUSER_USERNAME", "admin")
+email = os.environ.get("DJANGO_SUPERUSER_EMAIL", "admin@example.com")
+password = os.environ.get("DJANGO_SUPERUSER_PASSWORD")
+
+if password:
+    user, created = User.objects.get_or_create(
+        username=username,
+        defaults={
+            "email": email,
+            "is_staff": True,
+            "is_superuser": True,
+        }
+    )
+
+    user.email = email
+    user.is_staff = True
+    user.is_superuser = True
+    user.set_password(password)
+    user.save()
+
+    print("Superuser ready:", username)
+else:
+    print("DJANGO_SUPERUSER_PASSWORD not set. Skipping superuser creation.")
+PY
