ffe7ac2a220feffd4c9068215a05a0177617a4bd
sharathkumar <ssharathkumar033@gmail.com>
2026-05-15 21:51:13 +0530

    Fix site id for Render deployment

--- interview_mirror/settings.py
+++ interview_mirror/settings.py
@@ -68,8 +68,9 @@ INSTALLED_APPS = [
     'allauth.socialaccount',
     'allauth.socialaccount.providers.google',
 ]
-SITE_ID = 3
 
+
+SITE_ID = int(os.environ.get("SITE_ID", "1"))
 AUTHENTICATION_BACKENDS = [
     # Default Django login
     'django.contrib.auth.backends.ModelBackend',
